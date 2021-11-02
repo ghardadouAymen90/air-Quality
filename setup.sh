5056a6d6284d537fa06d8b23cdeb1b114d259207
a.ghardadou <aymen.ghardadou@sofrecom.com>
2021-11-02 02:19:18 +0100

    update

--- setup.sh
+++ setup.sh
@@ -3,21 +3,8 @@
 mongo <<EOF
    var cfg = {
         "_id": "rs0",
-        "version": 1,
-        "members": [
-            {
-                "_id": 0,
-                "host": "mongo-0.mongo:27017",
-                "priority": 2
-            },
-            {
-                "_id": 1,
-                "host": "mongo-1.mongo:27017",
-                "priority": 0
-            }
-        ]
     };
-    rs.initiate(cfg, { force: true });
+    rs.initiate(cfg);
     //rs.reconfig(cfg, { force: true });
     rs.status();
 EOF
